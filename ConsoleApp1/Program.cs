282ad33bd7e44aa17440a3a75a8f127f2cd66178
Andrville <andk@simcorp.com>
2024-11-04 14:35:00 +0200

    Third commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args) //test
         {
             Console.WriteLine("Hello world!");​
+            // Hi Shirkhan. Have a nice day.
         }
     }
 }
